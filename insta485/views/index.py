10aab0b30980b0c2399896f908c93da9e5b1941a
Jaslin <garciamj@umich.edu>
2026-07-15 01:51:03 -0400

    Add index.html, index.py, users.html, users.py. Fixed explore.html

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -1,6 +1,6 @@
 """Insta485 index view."""
-
 import flask
+import arrow
 import insta485
 
 
@@ -9,6 +9,61 @@ def show_index():
     """Display / route."""
     if "username" not in flask.session:
         return flask.redirect(flask.url_for("show_login"))
+    
+    logname = "awdeorio"
+    # Connect to database
+    connection = insta485.model.get_db()
+    con1 = connection.execute("""
+        SELECT DISTINCT U.username, U.filename
+        FROM users U""")
+    users = con1.fetchall()
 
-    context = {}
+    con2 = connection.execute("""
+        SELECT username
+        FROM users
+        WHERE username = ?""", (logname,))
+    logged_user = con2.fetchone()
+
+    con3 = connection.execute("""
+        SELECT P.postid, P.filename, P.owner, P.created
+        FROM posts P
+        JOIN following F ON P.owner = F.followee
+        WHERE F.follower = ?
+        UNION
+        SELECT P2.postid, P2.filename, P2.owner, P2.created
+        FROM posts P2
+        WHERE P2.owner = ?
+        ORDER BY P2.created DESC""", (logname, logname))
+    posts = con3.fetchall()
+
+    humantime_dict = {}
+    for post in posts:
+        time_created = str(post['created'])
+        arrow.get(time_created)
+        now = arrow.utcnow()
+        time_created = now.to('US/Eastern')
+        timestamp = time_created.humanize()
+        humantime_dict.update({post['postid']: timestamp})
+
+    con4 = connection.execute("""
+        SELECT postid, owner, COUNT(likeid) AS like_count
+        FROM likes
+        GROUP by postid""")
+    likes = con4.fetchall()
+
+    con5 = connection.execute("""
+        SELECT owner, postid, text
+        FROM comments
+        ORDER BY created ASC""")
+    comments = con5.fetchall()
+
+    # Add database info to context
+    context = {
+        "users": users,
+        "logged_user": logged_user,
+        "posts": posts,
+        "likes": likes,
+        "comments": comments,
+        "humantime_dict": humantime_dict
+    }
     return flask.render_template("index.html", **context)
---
+++ insta485/views/user.py
@@ -0,0 +1,72 @@
+"""Insta485 index (user) view."""
+import flask
+import insta485
+
+
+@insta485.app.route('/users/<user_url_slug>/',)
+def show_user(user_url_slug):
+    """Display / users."""
+    # Connect to database
+    connection = insta485.model.get_db()
+    logname = "awdeorio"
+
+    con = connection.execute(
+        "SELECT username, fullname  "
+        "FROM users "
+        "WHERE username = ?", (logname, ))
+    logged_user = con.fetchone()
+
+    con2 = connection.execute(
+        "SELECT username, fullname  "
+        "FROM users "
+        "WHERE username = ?", (user_url_slug, ))
+    us = con2.fetchone()
+
+    con3 = connection.execute(
+        "SELECT COUNT(postid) AS total_posts "
+        "FROM posts "
+        "WHERE owner = ? "
+        "GROUP BY owner ", (user_url_slug, ))
+    t_posts = con3.fetchone()
+
+    con4 = connection.execute("""
+        SELECT COUNT(follower) AS total_following
+        FROM following
+        WHERE follower = ?
+        GROUP BY follower""", (user_url_slug, ))
+    t_following = con4.fetchone()
+
+    con5 = connection.execute("""
+        SELECT COUNT(followee) AS total_followers
+        FROM following
+        WHERE followee = ?
+        GROUP BY followee""", (user_url_slug, ))
+    t_followers = con5.fetchone()
+
+    con6 = connection.execute("""
+        SELECT postid, filename
+        FROM posts
+        WHERE owner = ?""", (user_url_slug, ))
+    user_posts = con6.fetchall()
+
+    con7 = connection.execute("""
+        SELECT follower, followee
+        FROM following
+        WHERE follower = ? AND followee = ? """, (logname, user_url_slug, ))
+    user_fol = con7.fetchall()
+
+    if len(user_fol) != 0:
+        logname_follows_username = True
+    else:
+        logname_follows_username = False
+
+    context = {
+        "us": us,
+        "logged_user": logged_user,
+        "t_posts": t_posts,
+        "t_following": t_following,
+        "t_followers": t_followers,
+        "user_posts": user_posts,
+        "logname_follows_username": logname_follows_username
+    }
+    return flask.render_template("user.html", **context)
