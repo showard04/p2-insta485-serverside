9d4545a14765156db79a45551f4a0e55ce35788c
Jaslin <garciamj@umich.edu>
2026-07-16 04:15:03 -0400

    Add CSRF to all Forms. Update __init__.py

--- insta485/__init__.py
+++ insta485/__init__.py
@@ -1,10 +1,12 @@
 """Insta485 package initializer."""
 
 import flask
+from flask_wtf.csrf import CSRFProtect
 
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 app.config.from_object("insta485.config")
 app.config.from_envvar("INSTA485_SETTINGS", silent=True)
+csrf = CSRFProtect(app)
 
 import insta485.views  # noqa: E402  pylint: disable=wrong-import-position
 import insta485.model  # noqa: E402  pylint: disable=wrong-import-position
