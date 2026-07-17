254935a331ae78af2f72227b86e566cdf69c1173
Jaslin <garciamj@umich.edu>
2026-07-17 18:58:08 -0400

    Fixed all url_fors and uploads.py excluded abort error

--- insta485/views/uploads.py
+++ insta485/views/uploads.py
@@ -1,5 +1,5 @@
 """Upload routes."""
-
+import sqlite3
 import flask
 import insta485
 
@@ -10,6 +10,22 @@ def show_upload(filename):
     if "username" not in flask.session:
         flask.abort(403)
     # If an auth. user attempts to access a file that don't exist: abort(404)
+    connection = sqlite3.connect(insta485.app.config["DATABASE_FILENAME"])
+    connection.row_factory = sqlite3.Row
+    picture = connection.execute(
+        "SELECT filename FROM users WHERE filename = ?",
+        (filename,),
+    ).fetchall()
+
+    filee = connection.execute(
+        "SELECT filename FROM posts WHERE filename = ?",
+        (filename,),
+    ).fetchall()
+
+    if picture is None:
+        flask.abort(404)
+    if filee is None:
+        flask.abort(404)
 
     return flask.send_from_directory(
         insta485.app.config["UPLOAD_FOLDER"],
